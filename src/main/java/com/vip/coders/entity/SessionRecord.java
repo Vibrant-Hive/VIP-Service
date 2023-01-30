73e64aaf6965ac5b85e39241c6538483f21b64e6
Abdul Arsin <abdularsin@gmail.com>
2023-01-30 13:25:32 +0530

    Date Formats

--- src/main/java/com/vip/coders/entity/SessionRecord.java
+++ src/main/java/com/vip/coders/entity/SessionRecord.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.sql.Date;
 
 @Data
 @Getter
@@ -22,5 +23,5 @@ public class SessionRecord {
 
     String recordFileLink;
     String recordFileName;
-    String uploadedDate;
+    Date uploadedDate;
 }
