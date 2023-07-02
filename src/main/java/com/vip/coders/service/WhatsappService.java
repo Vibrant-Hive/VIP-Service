0a2548dcf96234e77bf38092bc4d7fe68b4c2f93
LENOVO <vijaymurali87@gmail.com>
2023-07-02 13:14:07 +0530

    Read chat file and returned map

--- src/main/java/com/vip/coders/service/WhatsappService.java
+++ src/main/java/com/vip/coders/service/WhatsappService.java
@@ -1,5 +1,6 @@
 package com.vip.coders.service;
 
+import com.vip.coders.model.WhatsAppChatItem;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -8,17 +9,44 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class WhatsappService {
 
-    public Double analyseWhatsappChat(int learnerId, int mentorId, MultipartFile chatFile) {
+    public Map<String, List<WhatsAppChatItem>> analyseWhatsappChat(int learnerId, int mentorId, MultipartFile chatFile) {
         List<String> fileContent = readFile(chatFile);
-        fileContent.forEach(s -> {
-            //your code goes here
+        List<WhatsAppChatItem> whatsAppChatItemList = new ArrayList<>();
+        fileContent.forEach( str -> {
+            if(str.contains(" - ")) {
+                String[] stringArr = str.split(" - ");
+                if (!stringArr[1].contains("end-to-end encrypted")) {
+                    if(stringArr[0].length() == 17 && stringArr[0].contains(":")) {
+                        WhatsAppChatItem chatItem = new WhatsAppChatItem();
+                        chatItem.setMessageTime(stringArr[0]);
+                        chatItem.setSenderName(stringArr[1].split(":")[0]);
+                        chatItem.setMessage(stringArr[1].split(":")[1]);
+
+                        whatsAppChatItemList.add(chatItem);
+                    }
+                }
+            }
         });
-        return null;
+
+        return convert(whatsAppChatItemList);
+    }
+
+    public Map<String, List<WhatsAppChatItem>> convert(List<WhatsAppChatItem> list) {
+        Map<String, List<WhatsAppChatItem>> map = new HashMap<>();
+        for (WhatsAppChatItem whatsAppChatItem : list) {
+            if(!map.containsKey(whatsAppChatItem.getSenderName())){
+                map.put(whatsAppChatItem.getSenderName(), new ArrayList<>());
+            }
+            map.get(whatsAppChatItem.getSenderName()).add(whatsAppChatItem);
+        }
+        return map;
     }
 
     private List<String> readFile(MultipartFile file) {
